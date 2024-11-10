2a7f92a3a0c6493d7e63498286c6341e413e0570
Gaziri-Arthur <arthurffgaziri@hotmail.com>
2024-11-10 20:40:42 -0300

    First and Last version

---
+++ SRCs/Bhaskara.py
@@ -0,0 +1,17 @@
+import math
+print('***Formulador de bhaskaralho***\n')
+a = float(input('Valor de a:'))
+b = float(input('Valor de b:'))
+c = float(input('Valor de c:'))
+D = b**2 - 4*a*c
+if (D) < 0:
+    print('não há raizes reais')
+else:
+    x1 = (-b + math.sqrt(D)) / (2*a)
+    x2 = (-b - math.sqrt(D)) / (2*a)
+    if (x1) == (x2):
+        print(x1)
+    else:    
+        print('\nValor de x1: {0}'.format(x1))
+        print('Valor de x2: {0}'.format(x2))
+
